6721160943f32623d5cbfbc504003224504c82ca
Jose Flores <jose.flores.152@gmail.com>
2017-04-26 18:34:39 -0400

    AWS environment variables and basic install commands

---
+++ install.aws.linux.server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+### AWS NODE SERVER INSTALL SCRIPT
+######
+
+## Update base
+
+sudo apt-get update
+sudo apt-get upgrade
+
+## Install System Software
+
+# Install aws cli
+sudo apt-get install awscli
+
+# jq json parser
+sudo apt-get install -y jq
+
+# Install NodeJS
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+## Add environment variables
+echo . $HOME/github/linux.aws.install/.aws_metadata.sh >> ~/.bashrc
