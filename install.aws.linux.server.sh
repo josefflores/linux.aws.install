08b4e3cf4cd7532876f4afd89ef8d4ed738eeb72
Jose Flores <jose.flores.152@gmail.com>
2017-04-26 20:33:53 -0400

    Install labels

--- install.aws.linux.server.sh
+++ install.aws.linux.server.sh
@@ -6,19 +6,31 @@
 ## Update base
 
 sudo apt-get update
-sudo apt-get upgrade
+
+# sudo apt-get upgrade
 
 ## Install System Software
 
 # Install aws cli
+printf " [INSTALL] AWS-CLI\n"
 sudo apt-get install -y awscli
 
 # jq json parser
+printf " [INSTALL] jq\n"
 sudo apt-get install -y jq
 
 # Install NodeJS
+printf "  [INSTALL] Node.js 7\n"
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 ## Add environment variables
-echo . $HOME/github/linux.aws.install/.aws_metadata.sh >> ~/.bashrc
+TARGET=$HOME/github/linux.aws.install/.aws_metadata.sh
+if grep -Fxq ". ${TARGET}" ~/.bashrc
+    printf " [INSTALL] AWS Environment variables\n"
+    echo . $TARGET >> ~/.bashrc
+else
+    printf " [SKIP]    AWS Environment variables previously added.\n"
+fi
+
+printf "Done...\n"
