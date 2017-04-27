3e984fb8df8b793058fe0f036acb960107207775
Jose Flores <jose.flores.152@gmail.com>
2017-04-26 20:07:44 -0400

    Seperating command into steps

--- .aws_metadata.sh
+++ .aws_metadata.sh
@@ -23,7 +23,7 @@ export META_RESERVATION_ID=$(curl -s ${URL_METADATA}reservation-id)
 export META_SEC_GROUP=$(curl -s ${URL_METADATA}security-groups)
 
 # Export tags to environment variables
-for prop in $(aws ec2 describe-tags --filters Name=resource-id,Values=${META_INSTANCE_ID}) | jq '[.Tags[] | "\(.Key)=\(.Value)"] | .[]' | sed 's/\"//g' | sed 's/[:space:]+/ /g')
+for prop in $(aws ec2 describe-tags --filters Name=resource-id,Values=${META_INSTANCE_ID} | jq '[.Tags[] | "\(.Key)=\(.Value)"] | .[]' | sed 's/\"//g' | sed 's/[:space:]+/ /g')
 do
     export $prop
 done
