769e7988d2ac4825df32285bc4d688d9b8b1aa7e
Jose Flores <jose.flores.152@gmail.com>
2017-05-04 13:21:40 -0400

    fixing AppendTofile by changing grep target

--- installer.sh
+++ installer.sh
@@ -6,7 +6,7 @@
 # $2 The line to add.
 # $3 The comment to display above the line and in script output.
 AppendToFile(){
-    INSTANCE=$(grep -c $2 $1)
+    INSTANCE=$(grep -c $3 $1)
     if (($INSTANCE >= 1))
     then
         printf " [SKIP]    $3.\n"
